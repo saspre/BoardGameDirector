82220952fbd1881a006fe11b6660353ab81e8939
pseudo <pseudo>
2013-08-03 11:05:40 +0200

    Added modules

---
+++ interface/src/com/dilph/bgd/front/BoardGameDirector.java
@@ -0,0 +1,15 @@
+package com.dilph.bgd.front;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BoardGameDirector extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
